141fe0a297b9fd2738e41c4fb88e7ecb309e3257
askery <askery@gmail.com>
2016-03-24 12:01:38 -0400

    Spli HH and MM

--- MinData.py
+++ MinData.py
@@ -9,25 +9,32 @@ THIS CODE IS TO COMPUTE MINUTE DATA FROM STOCK MARKET (BRAZILIAN STOCK LAYOUT AT
 
 from pyspark import SparkContext
 #1 ALL DAILY FILES ARE IN THE SAME FOLDER - JUST ADDRESSING THE FOLDER PATH IS ENOUGHT TO GATHER EVERYTHING
-logFile = "/media/data/rowData_201304_201508/NEG_20130920.TXT"  # Should be some file on your system
+logFile = "/media/data/rowData_201304_201512/NEG_20130920.TXT"  # Should be some file on your system
 sc = SparkContext("local", "Minute Data")
 logData = sc.textFile(logFile)
 
 #2 SELECTION OF STOCKS 
-stocks = ['PETR3', 'PETR4' ,'ABEV3', 'BBAS3', 'BBDC4', 'BVMF3', 'GGBR4', 'ITSA4', 'ITUB4','VALE3', 'VALE5']
+stocks = ['PETR3']
+#stocks = ['PETR3', 'PETR4' ,'ABEV3', 'BBAS3', 'BBDC4', 'BVMF3', 'GGBR4', 'ITSA4', 'ITUB4','VALE3', 'VALE5']
 def filterStock(line):
     return any(keyword in line for keyword in stocks)
 
 #3 IF WANT ALL STOCKS, COMMENT EVERYTHING BETWEEN #2 AND #3 AND UNCOMMENT EVERYTHING BETWEEN #3 AND #4
 # CODE TO ALL SOON
 
+#4 
+#hour_span = range(10,19) # MARKET OPEN AT 10 AND CLOSES AT 18
+#minu_span = range(60) # MINUTES
+
 #4 SELECTION OF SUITABLE COLUMNS AND KEY
 def makeColumns(line):
     pieces = line.split(';');   # SEPARATORS ARE ";"
     date   = pieces[0]          # FIRST COLUMN  - DATE
-    symb   = pieces [1].strip() # SECOND COLUMN - STOCK SYMBOL
-    hour   = pieces[5][:5]      # TRASANCTION TIME IN FORMAT HH:MM
-    key    = symb + date + hour # KEY DEFINITION AS CONCATENATION OF TIME COLUMNS
+    symb   = pieces[1].strip()  # SECOND COLUMN - STOCK SYMBOL
+    hour   = pieces[5][:2]      # EXTRACT HOUR FROM TRANSACTION TIME (HH:MM)
+    minu   = pieces[5][3:5]     # EXTRACT MINUTE (HH:MM)
+    key    = symb + date + hour + minu # KEY DEFINITION AS CONCATENATION OF TIME COLUMNS
+    #if hour in hour_span and 
     output = str("%.2f" % float(pieces[3])) + "   " + str(int(pieces[4])) # OUTOUT FORMAT WITHOUT 'U', ',' OR '()'
     return (key, (symb, (output) ))
 
@@ -39,4 +46,5 @@ def minData(a, b):
 result = logData.filter(filterStock).map(makeColumns).reduceByKey(minData).sortByKey()
 
 #7 OUTPUT IN TEXT FILE: ".coalesce(1)" PUT EVERYTHING IN JUST ONE OUTPUT FILE
-result.coalesce(1).saveAsTextFile('teste')
+ofile = symb + "teste"
+result.coalesce(1).saveAsTextFile(ofile)
